463d4f48368c2f379761df112f9df71d5df40f0b
EC2 Default User <ec2-user@ip-172-31-5-192.us-east-2.compute.internal>
2018-06-29 16:08:24 +0000

    collegeportal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,47 @@ require './config/environment'
 require './app/models/sample_model'
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+ configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
+ end
 
-  get '/' do
-    return erb :index
-  end
+ get '/' do
+   return erb :index
+ end
+post '/input' do
+ 
+   @answers = GPA.new(params)
+   @answers.to_f
+   @answers.calculate 
+   erb :unweighted_results
+ end
+ 
+ post '/GPA' do
+   erb :unweighted
+ end
+ 
+ post '/quiz' do
+   erb :quiz
+ end
+
+#  post '/results' do
+#   @new_answer = Answers.new(params)
+#   @new_answer.answer
+#   p @new_answer.answer
+#   erb :results #GPA
+#  end
+
+ post '/quiz_results' do
+     @new_answer = Answers.new(params)
+     @new_answer.answer
+     p @new_answer.answer
+     erb :quiz_results
+ end
+ 
+ 
+ post '/index' do
+   erb :index
+ end
+ 
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,156 @@
-class SampleModel
-  attr_accessor :name, :age
+class Unweighted_GPA
+  attr_accessor :number_classes
+  
+  def initialize(number_classes)
+      @number_classes=number_classes
+  end 
+end 
+
+
+
+class GPA
+
+@@total = 0
+@@grades = []
+
+def initialize(params)
+  @params = params
+end
+
+def to_f
+   @params.each do |value, grade|
+   @@grades << grade.to_f
+   
+   #@params = params
+   end
+end
+
+def grades_array
+   @@grades.to_s
+end
+   
+def calculate  
+   @@grades.each do |grade|
+       
+       if grade >= 90.0
+         @@total +=4.0
+     
+     
+       elsif grade >= 80.0
+         @@total+= 3.0
+     
+     
+       elsif grade >= 70.0
+         @@total+=2.0
+     
+     
+     else
+           @@total +=1.0
+       end
+   end
+   ((((@@total/6)/2).round(1))).to_s
+end
+
+def calculate_gpa
+end
+end 
+
+class Answers
+ attr_accessor :answer
+ def initialize(params)
+   @params = params
+   @answer = {
+     :Harvard => 0,
+     :Yale => 0,
+     :Princeton => 0,
+     :Columbia => 0
+     #:arts ==> 0
+     }
+ end
+
+ def location
+   if @params[:location] =="Cambridge, Massachusetts"
+     #do whatever in here
+     #:science = :science + 1
+     @answer[:Harvard] += 1
+     #taking in the hash and calling the key, science, and adding one point to it
+   elsif @params[:location] == "New Haven,Conneticut"
+     @answer[:Yale] +=1
+   elsif @params[:location] == "Princeton, New Jersey"
+     @answer[:Princeton] += 1
+   elsif @params[:location] == "New York"
+     @answer[:Columbia] += 1
+     #not using else bc dont wanna add a point
+   end
+ end
+
+ def major
+   if @params[:major] =="Economics"
+     @answer[:Harvard] += 1
+   elsif @params[:major] == "Political Science"
+     @answer[:Yale] +=1
+   elsif @params[:major] == "Computer Science"
+     @answer[:Princeton] += 1
+   elsif @params[:major] == "Psychology"
+     @answer[:Columbia] += 1
+   end
+ end
+
+ def alumni
+   if @params[:alumni] =="Barack Obama"
+     #do whatever in here
+     #:science = :science + 1
+     @answer[:Harvard] += 1
+     #taking in the hash and calling the key, science, and adding one point to it
+   elsif @params[:alumni] == "Meryl Streep"
+     @answer[:Yale] += 1
+   elsif @params[:alumni] == "John F. Kennedy "
+     @answer[:Princeton] += 1
+   elsif @params[:alumni] == "Alicia Keys"
+     @answer[:Columbia ]+= 1
+   end
+ end
+
+ def rate
+   if @params[:rate] =="5.3%"
+     #do whatever in here
+     #:science = :science + 1
+     @answer[:Harvard] += 1
+     #taking in the hash and calling the key, science, and adding one point to it
+   elsif @params[:rate] == "6.5%"
+     @answer[:Yale] +=1
+   elsif @params[:rate] == "7%"
+     @answer[:Princeton] += 1
+   elsif @params[:rate] == "6.1%"
+     @answer[:Columbia] += 1
+   end
+ end
+
+ def desc
+   if @params[:desc] =="Future Politician"
+     #do whatever in here
+     #:science = :science + 1
+     @answer[:Harvard] += 1
+     #taking in the hash and calling the key, science, and adding one point to it
+   elsif @params[:desc] == "Book-Lover"
+     @answer[:Yale] +=1
+   elsif @params[:desc] == "Preppy"
+     @answer[:Princeton] += 1
+   elsif @params[:desc] == "Tech Savvy"
+     @answer[:Columbia] += 1
+   end
+ end
+
+ def answer
+   location
+   major
+   alumni
+   rate
+   desc
+ end
+
+ def print
+   @answer
+ end
+
 end
